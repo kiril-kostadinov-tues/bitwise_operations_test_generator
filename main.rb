63be9690ad8f8522df900cba657fee7b32ff267f
kiril-kostadinov-tues <kiril.a.kostadinov@abv.bg>
2014-06-22 20:00:33 +0300

    Now working with templates

--- main.rb
+++ main.rb
@@ -8,76 +8,54 @@ class Task
 	end
 end
 
+def hex_length(l)
+	num = "0x#{rand(1..((16**l) - 1)).to_s(16)}"
+end
+
+def hex_range(range)
+	num = "0x#{rand(range).to_s(16)}"
+end
+
+def dec_length(l)
+	num = rand(1..((10**l) - 1)).to_s
+end
+
+def dec_range(range)
+	num = rand(range).to_s
+end
+
 class EasyTaskGenerator
 	def generate_task(number)
-		case number
-		when 1..2 
-			x = "a"
-			num = rand(15)
-			body = "int orig = 0x#{(num*(16**3) + num).to_s(16)};\n"
-			body +=	"int insert = 0x#{num.to_s(16)};\n"
-			body +=	"int a = orig | (insert << #{rand(1..12)});"
-		when 3..4
-			x = "AND"
-			num = rand(15)
-			body = "int orig = 0x#{(num*(16**3) + num).to_s(16)};\n"
-			body += "int insert = 0x#{num.to_s(16)};\n"
-			body += "int a = orig | (insert << #{rand(1..12)});\n"
-			body += "int b = orig | (insert << #{rand(1..12)});\n"
-			body += "int AND = a & b;"
-		when 5
-			x = "XOR"
-			num = rand(15)
-			body = "int orig = 0x#{(num*(16**3) + num).to_s(16)};\n"
-			body += "int insert = 0x#{num.to_s(16)};\n"
-			body += "int a = orig | (insert << #{rand(1..12)});\n"
-			body += "int b = orig | (insert << #{rand(1..12)});\n"
-			body += "int XOR = a ^ b;"
-		when 6
-			x = "left"
-			num = rand(15)
-			body = "int i = 0x#{(num*(16**3) + num).to_s(16)};\n"
-			body += "int left = i | (1 << #{rand(1..12)});"
-		when 7
-			x = "result"
-			num = rand(15)
-			body = "long value1 = 0x#{(num*(16**7) + num*(16**6) + num*(16**3) + num*(16**2)).to_s(16)};\n"
-			body += "long value2 = 0x#{(num*(16**7) + num*(16**4) + num*(16**3) + num).to_s(16)};\n"
-			body += "int result = (value1 << #{rand(1..4)}) ^ (value2 >> #{rand(1..4)});"
-		when 8
-			x = "result"
-			num = rand(15)
-			body = "long value1 = #{rand(1..512)};\n"
-			body += "long value2 = #{rand(1..512)};\n"
-			body += "int result = (value1 << #{rand(1..4)}) ^ (value2 >> #{rand(1..4)});"
-		when 9
-			x = "a"
-			num = rand(15)
-			body = "long testValue = 0x#{(num*(16**7) + num*(16**6) + num*(16**3) + num*(16**2)).to_s(16)};"
-			body += "a = 0;\n"
-			body += "if (testValue & (1 << #{rand(1..4)}))\n"
-			body += "{\na = 1;\n}\nelse\n{\na = 2;\n}"
-		when 10
-			x = "a"
-			num = rand(15)
-			body = "long testValue = 0x#{(num*(16**7) + num*(16**6) + num*(16**3) + num*(16**2)).to_s(16)};"
-			body += "int a = 0;\n"
-			body += "if (testValue & testValue ^ testValue | (1 << #{rand(1..4)}))\n"
-			body += "{\na = 1;\n}\nelse\n{\na = 2;\n}"
-		when 11
-			x = "result"
-			body = "int value1 = #{rand(1..512)};\n"
-			body += "int value2 = #{rand(1..512)};\n"
-			body += "int result = (value1 << #{rand(1..4)}) ^ (value2 >> #{rand(1..4)});"
-		when 12
-			x = "result"
-			body = "int value1 = #{rand(1..4096)};\n"
-			body += "int value2 = #{rand(1..4096)};\n"
-			body += "int result = (value1 << #{rand(1..4)}) | (value2 >> #{rand(1..4)});"
+		x = []
+		body = ""
+		File.open("easyTask#{number}.txt").each_with_index do |line, index|
+			if index == 0
+				x = line.gsub("\n", "").split(',')
+				next
+			end
+			body += line
 		end
+		num = rand(15)
+		body = eval('"' + body + '"')
+		Task.new(x, body)
+	end
+end
+
+class HardTaskGenerator
+	def generate_task(number)
+		x = []
+		body = ""
+		File.open("hardTask#{number}.txt").each_with_index do |line, index|
+			if index == 0
+				x = line.gsub("\n", "").split(',')
+				next
+			end
+			body += line
+		end
+		body = eval('"' + body + '"')
 		Task.new(x, body)
 	end
 end
 
 generator = EasyTaskGenerator.new
-p generator.generate_task(12)
+p generator.generate_task(1)
