f32c9e746732ac8f083a93359a179fbb210257a6
Bozhidar Nikolov <bobsan_1996@abv.bg>
2014-06-23 23:35:21 +0300

    Script now generates more structured html files.

--- main.rb
+++ main.rb
@@ -12,36 +12,58 @@ end
 
 class HtmlGenerator
 
-	attr_accessor :file_name
+	attr_accessor :number
 
-	def initialize(filename)
-		@file_name = filename
+	def initialize(number)
+		@number = number
 	end
 
-	def generate_html(tasks)
+	def generate_html_test(tasks)
 		builder = Nokogiri::HTML::Builder.new do |doc|
 
 		doc.html {
+			doc.head {
+				doc.title {
+					doc.text "TEST #{number}"
+				}
+				doc.link(:rel => 'stylesheet', :type => 'text/css', :href => './style.css')
+			}
 			doc.body {
-				tasks.each do |current_task|
-					doc.div {
-						current_task.x.each do |current|
-							doc.text "#{current}, "
-						end
-						doc.text "= ?"
-						doc.br
-						current_task.body.each_char do |i|
-							doc.text i
-							if i == ';'
-							doc.br
-						end		
-						end
-					}
-				end
+				task_number = 1
+				doc.table {
+					tasks.each_slice(2) do |slice|
+						doc.tr {
+							slice.each do |data|
+								doc.td {
+									doc.b.task_number {
+										doc.text task_number
+									}
+									task_number += 1
+									x = ""
+									data.x.each_with_index do |current, index|
+										x += index == 0 ? "#{current} = ?" : ", #{current} = ?"
+									end
+									doc.b.x {
+										doc.text x
+									}
+									doc.br
+									doc.code {
+										data.body.each_char do |i|
+											doc.text i
+											if i == ';' or i == '{' or i == '}'
+												doc.br
+											end		
+										end
+									}
+								}
+							end
+						}
+					end
+				}
 			}
 		}
 		end
-		File.open(file_name, "w") { |file| file.write(builder.to_html) }
+		File.open("Test#{number}.html", "w") { |file| file.write(builder.to_html) }
 	end
 end
 
@@ -97,11 +119,10 @@ end
 tasks = []
 easy_task_generator = EasyTaskGenerator.new
 hard_task_generator = HardTaskGenerator.new
-html_generator = HtmlGenerator.new "index.html"
+html_generator = HtmlGenerator.new 1
 for i in 1..12
 	tasks << hard_task_generator.generate_task(i)
-end	
-html_generator.generate_html(tasks)
-
+end
+html_generator.generate_html_test(tasks)
 
 
