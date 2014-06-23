7e0819df865dc4cac5508dff925bd530616dc64f
kiril-kostadinov-tues <kiril.a.kostadinov@abv.bg>
2014-06-24 02:32:31 +0300

    Lots of changes
    Organized files, Generating answers, Generating PDF, Minor improvements
    to templates

--- main.rb
+++ main.rb
@@ -3,10 +3,19 @@ require "nokogiri"
 class Task
 	attr_accessor :x
 	attr_accessor :body
+	attr_accessor :answer
 
 	def initialize(x, body)
 		@x = x
 		@body = body
+		@answer = []
+	end
+
+	def get_answer
+		`gcc test.c`
+		`./a.out`.each_line do |line|
+			@answer << [line.gsub("\n", ""), "0x#{line.to_i.to_s(16)}"]
+		end
 	end
 end
 
@@ -65,6 +74,34 @@ class HtmlGenerator
 		end
 		File.open("Test#{number}.html", "w") { |file| file.write(builder.to_html) }
 	end
+
+	def generate_html_answers(tasks)
+		builder = Nokogiri::HTML::Builder.new do |doc|
+
+		doc.html {
+			doc.head {
+				doc.title {
+					doc.text "TEST #{number} ANSWERS"
+				}
+				doc.link(:rel => 'stylesheet', :type => 'text/css', :href => './style.css')
+			}
+			doc.body {
+				task_number = 1
+				tasks.each do |task|
+					doc.b.task_number {
+						doc.text task_number
+					}
+					task_number += 1
+					task.answer.each_with_index do |answer, index|
+						doc.text "#{task.x[index]} = #{answer[0]}, #{answer[1]}"
+						doc.br
+					end
+				end
+			}
+		}
+		end
+		File.open("Test#{number}_answers.html", "w") { |file| file.write(builder.to_html) }
+	end
 end
 
 class CFilesGenerator 
@@ -109,14 +146,14 @@ class EasyTaskGenerator
 	def generate_task(number)
 		x = []
 		body = ""
-		File.open("easyTask#{number}.txt").each_with_index do |line, index|
+		File.open("Templates/easyTask#{number}.txt").each_with_index do |line, index|
 			if index == 0
 				x = line.gsub("\n", "").split(',')
 				next
 			end
 			body += line
 		end
-		num = rand(15)
+		num = rand(1..15)
 		body = eval('"' + body + '"')
 		Task.new(x, body)
 	end
@@ -126,27 +163,38 @@ class HardTaskGenerator
 	def generate_task(number)
 		x = []
 		body = ""
-		File.open("hardTask#{number}.txt").each_with_index do |line, index|
+		File.open("Templates/hardTask#{number}.txt").each_with_index do |line, index|
 			if index == 0
 				x = line.gsub("\n", "").split(',')
 				next
 			end
 			body += line
 		end
+		num = rand(1..15)
 		body = eval('"' + body + '"')
 		Task.new(x, body)
 	end
 end
 
-tasks = []
-easy_task_generator = EasyTaskGenerator.new
-hard_task_generator = HardTaskGenerator.new
-html_generator = HtmlGenerator.new 1
-for i in 1..12
-	tasks << hard_task_generator.generate_task(i)
-	c_file_generator = CFilesGenerator.new "current.c"
-	c_file_generator.generate_c_file tasks[i - 1]
+generator = ARGV[1] == "2" ? HardTaskGenerator.new : EasyTaskGenerator.new
+ARGV[0].nil? ? number_of_tests = 0 : number_of_tests = ARGV[0].to_i - 1
+ARGV[2].nil? ? starting_test = 1 : starting_test = ARGV[2].to_i
+c_file_generator = CFilesGenerator.new("test.c")
+
+for i in starting_test..starting_test+number_of_tests
+	tasks = []
+	html_generator = HtmlGenerator.new(i)
+	for j in 1..12
+		tasks << generator.generate_task(j)
+		c_file_generator.generate_c_file(tasks[j-1])
+		tasks[j-1].get_answer
+	end
+	html_generator.generate_html_test(tasks)
+	html_generator.generate_html_answers(tasks)
+	`./Converter/wkhtmltopdf Test#{i}.html Test#{i}.pdf`
+	`./Converter/wkhtmltopdf Test#{i}_answers.html Test#{i}_answers.pdf`
+	puts "GENERATED TEST #{i}"
 end
-html_generator.generate_html_test(tasks)
 
-
+`rm -f ./a.out`
+`rm -f ./test.c`
